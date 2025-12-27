d1b28db8e1553120a789f856de77069dc88a0a19
Farhaan Kaleem <farhaankaleem275@gmail.com>
2025-12-27 10:57:31 +0530

    for loops

---
+++ basics/loops.java
@@ -0,0 +1,23 @@
+public class loops {
+    public static void main(String[] args) {
+        
+        // //counter++ => counter = counter +1
+
+        
+        // for(int counter=0;counter<11;counter++){
+        //  System.out.print(counter + " ");
+        // }
+
+           
+
+        // Print sum of first 4 natural no.
+
+        int sum=0;
+        for(int i=1;i<=4;i++){
+            sum = sum +i;
+        }
+        System.out.println(sum);
+        
+
+    }
+}
