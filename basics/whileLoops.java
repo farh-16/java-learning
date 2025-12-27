357ca433a92076df67bd82b227fee5e5c5627d03
Farhaan Kaleem <farhaankaleem275@gmail.com>
2025-12-27 10:59:44 +0530

    while loops

---
+++ basics/whileLoops.java
@@ -0,0 +1,36 @@
+public class whileLoops{
+    public static void main(String[] args) {
+
+        //  int i =0;
+        //  while(i<11){
+        //     System.out.println(i);
+        //     i++;
+        //  }
+
+       
+
+        //   DO WHILE
+
+    //    int i =0;
+    //    do{
+    //     System.out.println(i);
+    //     i=i+1;
+    //    }while(i<11);
+     
+
+        // while and do while
+
+
+        int i =18;
+        while(i<12){
+            System.out.println("learning java");
+            i++;
+        }
+
+        do { 
+            System.out.println("learning java");
+            i++;
+        } while (i<12);
+
+    }
+}
